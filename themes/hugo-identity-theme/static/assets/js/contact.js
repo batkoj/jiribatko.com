bea876bf1fd6142317b8a10fb80fcbb337533eb4
Jiri Batko <jiri.batko@gmail.com>
2018-04-07 17:28:10 -0700

    Fix sending email

--- themes/hugo-identity-theme/static/assets/js/contact.js
+++ themes/hugo-identity-theme/static/assets/js/contact.js
@@ -1,36 +1,49 @@
 // Initial form validation
-$(document).ready(function() {
+$(document).ready(function () {
   $.validate({
-    modules : 'html5, toggleDisabled'
+    modules: 'html5, toggleDisabled'
   });
 });
 
 // Revalidate on change
-$('form[id=contactForm]').change(function() {
+$('form[id=contactForm]').change(function () {
   $.validate({
-    modules : 'html5, toggleDisabled'
+    modules: 'html5, toggleDisabled'
   });
 });
 
 // Async contact form
-$('form[id=contactForm]').submit(function(){
-  $('form[id=contactForm] #success').hide();
-  $('form[id=contactForm] #error').hide();
+// $('form[id=contactForm]').submit(function () {
+//   $('form[id=contactForm] #success').hide();
+//   $('form[id=contactForm] #error').hide();
 
-  // AJAX POST, with contact form data serialized, which expects a JSON response
-  // Datatype must be set to 'json' to use formspree.io
-  $.post($(this).attr('action'), $(this).serialize(), function(){
-      $('form[id=contactForm] #success').show();
-    }, 'json').fail(function(){
-      $('form[id=contactForm] #error').show();
-  });
-  return false;
-});
+//   // AJAX POST, with contact form data serialized, which expects a JSON response
+//   // Datatype must be set to 'json' to use formspree.io
+//   $.post($(this).attr('action'), $(this).serialize(), function () {
+//     $('form[id=contactForm] #success').show();
+//   }, 'json').fail(function () {
+//     $('form[id=contactForm] #error').show();
+//   });
+//   return false;
+// });
+
+// $('form[id=contactForm]').submit(function (event) {
+//   $('form[id=contactForm] #success').hide();
+//   $('form[id=contactForm] #error').hide();
+
+  // if ( $( "input:first" ).val() === "correct" ) {
+    // $('form[id=contactForm] #success').show();
+    // return;
+  // }
+ 
+  // $('form[id=contactForm] #error').show();
+  // event.preventDefault();
+// });
 
 // Callback function for captcha
 function onSubmit(token) {
   if (!token) {
-     return;
+    return;
   }
   $('form[id=contactForm]').submit();
 }
